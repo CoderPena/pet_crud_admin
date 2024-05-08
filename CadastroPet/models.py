9612923b46e22bb01d04592f4b12084f19943c0d
Marcelo Pena <coder.pena@gmail.com>
2024-05-08 07:31:06 -0300

    Ajustes na classe de PET

--- CadastroPet/models.py
+++ CadastroPet/models.py
@@ -34,19 +34,54 @@ class Raca(models.Model):
         return str(self.raca_id) + ' | ' + self.nome
     
 class Pet(models.Model):
+    OPCOES_ESPECIE = [
+        ('C', 'Canina'),
+        ('F', 'Felina'),
+    ]
     OPCOES_PORTE = [
-        ('P', 'Pequeno'),
+        ('P', 'Pequeno'),   
         ('M', 'Médio'),
         ('G', 'Grande'),
     ]
+    OPCOES_SIM_NAO = [
+        ('S', 'Sim'),
+        ('N', 'Não'),
+    ]
+    OPCOES_ADOCAO = [
+        ('D', 'Disponível'),
+        ('A', 'Adotado'),
+        ('E', 'Em adaptação'),
+        ('N', 'Não disponível'),
+    ]
+    OPCOES_TEMPERAMENTO = [
+        ('N', 'Normal'),
+        ('H', 'Hiperativo'),
+        ('M', 'Medroso'),
+        ('A', 'Agressivo'),
+        ('S', 'Manso'),
+        ('C', 'Sociável com outros animais'),
+        ('O', 'Outros'),
+    ]
 
     pet_id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100)
-    raca_id = models.ForeignKey(Raca, on_delete=models.SET_NULL, null=True)
-    porte = models.CharField(max_length=1, choices=OPCOES_PORTE)
-    data_nascimento = models.DateField()
-    foto = models.ImageField(upload_to='fotos_pet/', blank=True, null=True)
-    historico = models.TextField(max_length=5000, default='N/A')
+    nome = models.CharField(max_length=100,verbose_name='Nome do Pet')
+    nome_ninhada = models.CharField(max_length=100, default='N/A', verbose_name='Nome da ninhada', help_text="Digite o nome da ninhada a que pertente.")
+    especie = models.CharField(max_length=1, choices=OPCOES_ESPECIE, verbose_name='Espécie')
+    raca_id = models.ForeignKey(Raca, on_delete=models.SET_NULL, null=True, verbose_name='ID da raça')
+    porte = models.CharField(max_length=1, choices=OPCOES_PORTE, verbose_name='Porte do animal')
+    data_nascimento = models.DateField(verbose_name='Data do nascimento')
+    foto = models.ImageField(upload_to='fotos_pet/', blank=True, null=True, verbose_name='Foto do crachá')
+    historico = models.TextField(max_length=5000, default='N/A', verbose_name='Histórico', help_text="Digite os antecedentes do animal e características de personalidade e comportamentais.")
+    data_chegada = models.DateField(verbose_name='Data de Chegada na ONG')
+
+    divulga = models.CharField(max_length=1, choices=OPCOES_SIM_NAO, verbose_name='Divulgação', help_text="Permite texto de divulgação externa?")
+    texto_divulga = models.TextField(max_length=5000, default='N/A', verbose_name='Texto para divulgação', help_text="Digite o texto para divulgação externa.")
+    temperamento = models.CharField(max_length=1, choices=OPCOES_TEMPERAMENTO, verbose_name='Temperamento do animal')
+    doencas = models.TextField(max_length=5000, default='N/A', help_text="Digite as doenças do animal.")
+
+    status_adocao = models.CharField(max_length=1, choices=OPCOES_ADOCAO)
+    data_obito = models.DateField(null=True, verbose_name='Data do óbito')
+    motivo_obito = models.CharField(max_length=100, null=True, verbose_name='Motivo óbito')
 
     def __str__(self):
         return str(self.pet_id) + ' | ' + self.nome
